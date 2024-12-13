70a82ec4772b0b4db686f27302f98952e793efb3
lydiadaids <lydiadaids7@gmail.com>
2024-12-12 23:53:13 -0500

    Update test_user_service.py

--- tests/test_services/test_user_service.py
+++ tests/test_services/test_user_service.py
@@ -66,7 +66,7 @@ async def test_update_user_valid_data(db_session, user):
     new_email = "updated_email@example.com"
     updated_user = await UserService.update(db_session, user.email, user.id, {"email": new_email})
     assert isinstance(updated_user, User), "Update function should return a User object or None"
-    assert updated_user.email == new_email, "Email should be updated to a new value"
+    assert updated_user.email == new_email, "Email should be updated to the new value"
 
 # Test updating a user with invalid data
 async def test_update_user_invalid_data(db_session, user):
@@ -84,14 +84,6 @@ async def test_delete_user_does_not_exist(db_session):
     non_existent_user_id = "non-existent-id"
     deletion_success = await UserService.delete(db_session, non_existent_user_id)
     assert deletion_success is False
-
-# Test listing users with pagination
-async def test_list_users_with_pagination(db_session, users_with_same_role_50_users):
-    users_page_1 = await UserService.list_users(db_session, skip=0, limit=10)
-    users_page_2 = await UserService.list_users(db_session, skip=10, limit=10)
-    assert len(users_page_1) == 10
-    assert len(users_page_2) == 10
-    assert users_page_1[0].id != users_page_2[0].id
 
 # Test registering a user with valid data
 async def test_register_user_with_valid_data(db_session, email_service):
@@ -173,6 +165,7 @@ async def test_create_user_with_valid_data_test6(db_session, email_service):
     user = await UserService.create(db_session, user_data, email_service)
     assert user is not None
     assert user.role == UserRole.ADMIN
+
 async def test_create_user_with_valid_data_test7(db_session, email_service):
     user_data = {
         "nickname": generate_nickname(),
