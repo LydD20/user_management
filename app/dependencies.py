5e6ebdab64e884a25566260c25e8d43e19481c25
Lydia Daids <lydiadaids7@gmaill.com>
2024-12-12 22:15:01 -0500

    added endpoints for upgrading to premium and user profile

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,7 @@ from builtins import Exception, dict, str
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user_model import User
 from app.database import Database
 from app.utils.template_manager import TemplateManager
 from app.services.email_service import EmailService
